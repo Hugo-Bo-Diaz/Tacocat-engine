8afed6b6154aca628f3e8c8468b8febd4587303b
Hugo-Bo-Diaz <hugobodiaz@gmail.com>
2018-09-15 19:32:10 +0200

    working
    needs a ton of cleaning

--- Sources/ModuleUI.h
+++ Sources/ModuleUI.h
@@ -15,7 +15,7 @@ public:
 	bool Init();
 	update_status PreUpdate(float dt);
 	update_status Update(float dt);
-	bool CleanUp();
+	bool CleanUp() { return true; };
 
 private:
 	//list of ui elements
