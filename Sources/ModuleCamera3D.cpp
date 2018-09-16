d33346a929a99b7f5a2a5782da7cb7f014cf171a
Hugo-Bo-Diaz <hugobodiaz@gmail.com>
2018-09-17 00:09:13 +0200

    small changes

--- Sources/ModuleCamera3D.cpp
+++ Sources/ModuleCamera3D.cpp
@@ -152,6 +152,9 @@ float* ModuleCamera3D::GetViewMatrix()
 // -----------------------------------------------------------------
 void ModuleCamera3D::CalculateViewMatrix()
 {
-	ViewMatrix = mat4x4(X.x, Y.x, Z.x, 0.0f, X.y, Y.y, Z.y, 0.0f, X.z, Y.z, Z.z, 0.0f, -dot(X, Position), -dot(Y, Position), -dot(Z, Position), 1.0f);
+	ViewMatrix = mat4x4(	X.x,				Y.x,				Z.x,				0.0f, 
+							X.y,				Y.y,				Z.y,				0.0f,
+							X.z,				Y.z,				Z.z,				0.0f, 
+							-dot(X, Position),	-dot(Y, Position),	-dot(Z, Position),	1.0f);
 	ViewMatrixInverse = inverse(ViewMatrix);
 }
