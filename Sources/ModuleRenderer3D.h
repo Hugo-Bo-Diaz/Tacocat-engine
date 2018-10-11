a660250c02ccebabe01d52ade523eda7bfd9f7f5
Hugo-Bo-Diaz <hugobodiaz@gmail.com>
2018-10-11 11:41:08 +0200

    this didn't commit

--- Sources/ModuleRenderer3D.h
+++ Sources/ModuleRenderer3D.h
@@ -84,9 +84,11 @@ public:
 	NOTlinder* AddCylinder(float radius, float sector, float stacks, float height, float x = 0, float y = 0, float z = 0);
 	NOTarrow* AddArrow(float x1,float y1, float z1, float x2, float y2, float z2);
 	void AddElement(NOTprimitive*);
+	void AddMesh(NOTmesh*);
 
 	std::vector<NOTprimitive*> primitive_vector;
-	
+	std::vector<NOTmesh*> mesh_vector;
+
 	GLuint texture_buffer =0;
 	GLubyte checkImage[100][100][4];
 
