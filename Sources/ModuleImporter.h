a40f05f00b12d7e5e43688fa6ac0f08fb9f3b06d
Hugo-Bo-Diaz <hugobodiaz@gmail.com>
2018-10-23 12:46:47 +0200

    solved very small crash

--- Sources/ModuleImporter.h
+++ Sources/ModuleImporter.h
@@ -8,17 +8,17 @@
 class ModuleImporter : public Module
 {
 public:
-	ModuleImporter(bool start_enabled = true);
-	~ModuleImporter();
+	ModuleImporter(bool start_enabled = true) {};
+	~ModuleImporter() {};
 
 	MeshImporter* mesh = nullptr;
 	
-	bool Start();
-	bool CleanUp();
+	bool Start() { return true; };
+	bool CleanUp() { return true; };
 
-	void Load(const char* filename, Scene* scene_to);
+	void Load(const char* filename, Scene* scene_to) {};
 
-	void FocusCamera();
+	void FocusCamera() {};
 
 private:
 
