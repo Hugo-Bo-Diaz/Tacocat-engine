248561fb2f829c518f0c2927dad1ef78ad5484b6
Daraenor <viriato22@gmail.com>
2018-11-05 22:42:57 +0100

    Update on the transform component
    Fixed a mistake that has been bothering me for weeks

--- Sources/TransformComponent.cpp
+++ Sources/TransformComponent.cpp
@@ -9,40 +9,20 @@ TransformComponent::~TransformComponent()
 {
 }
 
-void TransformComponent::Move(float3 newpos)
-{
-	pos += newpos;
-}
-
-void TransformComponent::Rotate(float3 newrot)
-{
-	quaternion.FromEulerXYZ(rot.x, rot.y, rot.z);
-
-	/*Quat X, Y, Z;
-
-	X.FromEulerXYZ(newrot.x, 0.0f, 0.0f);
-	Y.FromEulerXYZ(0.0f, newrot.y, 0.0f);
-	Z.FromEulerXYZ(0.0f, 0.0f, newrot.z);*/
-
-	quaternion.RotateX(newrot.x);
-	quaternion.RotateY(newrot.y);
-	quaternion.RotateZ(newrot.z);
-
-	rot = quaternion.ToEulerXYZ();
-}
-
 void TransformComponent::SetPos(float x, float y, float z)
 {
 	pos.x = x;
 	pos.y = y;
 	pos.z = z;
+
+	Update();
 }
 
 void TransformComponent::SetRot(float x, float y, float z)
 {
-	rot.x = x;
-	rot.y = y;
-	rot.z = z;
+	rot = rot.FromEulerXYZ(x, y, z);
+	
+	Update();
 }
 
 void TransformComponent::SetScale(float x, float y, float z)
@@ -50,5 +30,16 @@ void TransformComponent::SetScale(float x, float y, float z)
 	scale.x = x;
 	scale.y = y;
 	scale.z = z;
+
+	Update();
 }
 
+void TransformComponent::Update()
+{
+	transform = transform.FromTRS(pos, rot, scale);
+}
+
+float4x4 TransformComponent::GetTransform()
+{
+	return transform;
+}
--- Sources/TransformComponent.h
+++ Sources/TransformComponent.h
@@ -10,19 +10,20 @@ public:
 	TransformComponent();
 	~TransformComponent();
 
-	void Move(float3 newpos);
-	void Rotate(float3 newrot);
-
 	void SetPos(float x, float y, float z);
 	void SetRot(float x, float y, float z);
 	void SetScale(float x, float y, float z);
 
+	void Update();
+
+	float4x4 GetTransform();
+
 private:
 	float3 pos;
-	float3 rot;
+	Quat rot;
 	float3 scale;
 
-	Quat quaternion;
+	float4x4 transform;
 };
 
 #endif
