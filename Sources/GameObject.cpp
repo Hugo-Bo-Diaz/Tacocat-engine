50676fb2bc2197f443fa43504db1cfe81a1b6bb0
Daraenor <viriato22@gmail.com>
2018-11-13 12:49:27 +0100

    Added name to hierarchy

--- Sources/GameObject.cpp
+++ Sources/GameObject.cpp
@@ -155,7 +155,7 @@ void GameObject::Hierarchy()
 	if (children.empty()) flags |= ImGuiTreeNodeFlags_Leaf;
 	if (App->scene_controller->current_scene->id == UID) flags |= ImGuiTreeNodeFlags_Selected;
 
-	if (ImGui::TreeNodeEx("", flags))
+	if (ImGui::TreeNodeEx(name.c_str(), flags))
 	{
 		if (ImGui::IsItemClicked())
 			App->scene_controller->current_scene->spookamera->selected = this;
