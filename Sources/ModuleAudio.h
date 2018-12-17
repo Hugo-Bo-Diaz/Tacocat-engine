e5eb60aafe056756e74a1ae1e3e87d9e2873c28a
Daraenor <viriato22@gmail.com>
2018-12-17 19:20:31 +0100

    Patch module audio
    It doesn't crash the engine

--- Sources/ModuleAudio.h
+++ Sources/ModuleAudio.h
@@ -4,10 +4,38 @@
 #include "Module.h"
 #include "Application.h"
 
-//#include "WWISESDK/AK/SoundEngine/Common/AkSoundEngine.h"
-//#include "WWISESDK/AK/IBytes.h"
-//#include <AK/SoundEngine/Common/AkSoundEngine.h>
-//#include <AK/IBytes.h>
+#include <AK/SoundEngine/Common/AkSoundEngine.h>
+#include <AK/MusicEngine/Common/AkMusicEngine.h>	  
+#include <AK/SoundEngine/Common/AkMemoryMgr.h>    
+#include <AK/SoundEngine/Common/AkModule.h>         
+#include <AK/SoundEngine/Common/AkStreamMgrModule.h>
+
+#include <AK/Tools/Common/AkPlatformFuncs.h>
+
+
+// Input libraries
+#ifndef _DEBUG  // Profile build configuration must be loaded instead of Debug
+#define AK_OPTIMIZED
+#pragma comment( lib, "Wwise/lib/Release/lib/AkSoundEngine.lib")
+#pragma comment( lib, "Wwise/lib/Release/lib/AkMusicEngine.lib")
+#pragma comment( lib, "Wwise/lib/Release/lib/AkMemoryMgr.lib")
+#pragma comment( lib, "Wwise/lib/Release/lib/AkStreamMgr.lib")
+#pragma comment( lib, "Wwise/lib/Release/lib/AkRoomVerbFX.lib")
+#else
+#include <AK/Comm/AkCommunication.h>
+#pragma comment( lib, "Wwise/lib/Debug/lib/CommunicationCentral.lib")
+#pragma comment( lib, "Wwise/lib/ws2_32.lib") 
+#pragma comment( lib, "Wwise/lib/Debug/lib/AkSoundEngine.lib")
+#pragma comment( lib, "Wwise/lib/Debug/lib/AkMusicEngine.lib")
+#pragma comment( lib, "Wwise/lib/Debug/lib/AkMemoryMgr.lib")
+#pragma comment( lib, "Wwise/lib/Debug/lib/AkStreamMgr.lib")
+#pragma comment( lib, "Wwise/lib/Debug/lib/AkRoomVerbFX.lib")
+#endif
+
+// Microsoft DirectX external libs
+#pragma comment( lib, "Wwise/lib/dinput8.lib")
+#pragma comment( lib, "Wwise/lib/dsound.lib") 
+#pragma comment( lib, "Wwise/lib/dxguid.lib")  
 
 /////////////////////////////////////////////////////////////////////////////////
 // Custom alloc/free functions. These are declared as "extern" in AkMemoryMgr.h
